d5657a3c6fbd10af6f20d0db37aeba166f586ae1
David Silva <dedeco89@gmail.com>
2023-04-02 15:57:36 -0400

    first commit

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    LightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLable: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Car',
+    dark: true, 
+    primary: true,
+    darkText: false
+}; 
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    LightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Access your account anywhere at any time',
+    description: 'No matter where you are, you will always have access to your account with just a few taps of your screen.',
+    buttonLable: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg').default,
+    alt: 'mobileApp',
+    dark: false, 
+    primary: false,
+    darkText: true
+}; 
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    LightTextDesc: false,
+    topLine: 'Join the Family',
+    headline: "Let's get your account setup ",
+    description: 'With just a few taps of your screen, you will be on your way to creating your account',
+    buttonLable: 'Start Now',
+    imgStart: true,
+    img: require('../../images/svg-5.svg').default,
+    alt: 'Car',
+    dark: false, 
+    primary: false,
+    darkText: true
+}; 
